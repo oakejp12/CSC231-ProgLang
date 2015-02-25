fa1f6526744a9d9df2ad31491a24feb6c146b33c
Johan Oakes <oakejp12@wfu.edu>
2015-02-25 03:18:08 -0500

    Work on largest(L) function

--- lab6.erl
+++ lab6.erl
@@ -1,7 +1,7 @@
 % Author: Johan Oakes
 % Lab 6 Functions in Erlang
 -module (lab6).
--export ([productOfPairs/1, polyadd/2, polydiff/2, polyeval/2]).
+-export ([productOfPairs/1, polyadd/2, polydiff/2, polyeval/2, member/2, delete/2, insert/2]).
 
 % test1()
 % Allows you to multiply consecutive elements in a lists
@@ -14,11 +14,11 @@ productOfPairs([X]) ->
 productOfPairs([X | XS]) ->
 	[X * hd(XS) | productOfPairs(tl(XS))].
 
-% 
+% -----------------------------------------
 % All poly_ recurvsive functions below: 
 % 	Use patterns in place of hd & tl calls
 %	Cannot use local variables
-%
+% -----------------------------------------
 
 % test2a() 
 % Add two polynomials together w/ certain constraints
@@ -51,13 +51,31 @@ polyeval([C | CS], N) ->
 	C + (N * polyeval(CS, N)).
 
 
-			
+% -----------------------------------------
+% Basic implementations of sets using lists
+% Set definition: items in the set may appear
+%	in any order in the underlying list, but only once
+% Use patterns and local variables as approp.
+% -----------------------------------------
+
+% test3a()
+% member(X,S) -> true if element X is in the set S.
+
+
+% test3b()
+% delete(X,S) -> removes x from the set S if X is present in the set
+
+
+% test3c()
+% insert(X,S) -> add X to set S if X is not already present in set
 
 
 
-
-
-
+%------------------------------------------
+% Rewrite of the largest function
+% Use local variables (not patterns) to
+%	reduce the number of times hd & tl are called
+%------------------------------------------
 
 
 
