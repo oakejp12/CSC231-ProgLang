019721e9332c7feff00166ae87a1bbca5d70d334
Johan Oakes <oakejp12@wfu.edu>
2015-03-03 01:16:31 -0500

    Finish implementation of longStrings(L)

--- Lab7/lab7.erl
+++ Lab7/lab7.erl
@@ -1,5 +1,5 @@
 -module(lab7).
--export([logicalOR/1, extractAWords/1, extractEvens/1, map/2,reduce/2,filter/2,getStringTree/0,stringToFloat/1,getExpression1/0,getExpression2/0,getExpression3/0]).
+-export([longStrings/1, logicalOR/1, extractAWords/1, extractEvens/1, map/2,reduce/2,filter/2,getStringTree/0,stringToFloat/1,getExpression1/0,getExpression2/0,getExpression3/0]).
 
 -record(binaryTreeNode, {value,left=null,right=null}).
 -record(binaryTree, {rootNode=null}).
@@ -78,5 +78,20 @@ logic(X,Y) ->
 % Find the logical OR of a list of Boolean values.
 logicalOR(L) ->
 	reduce(fun logic/2, L).
+
+
+% Check if string is longer than 3 characters
+isLong(S) ->
+	if
+		length(S) > 3 -> true;
+		true -> false
+	end.
+
+
+% Filter out elements of a list longer than 3 characters
+longStrings(List) ->
+	filter(fun isLong/1, List).
+
+
 
 
